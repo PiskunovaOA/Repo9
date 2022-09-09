1691487a48244cae6a32e846b5e2535ba73b9555
Olga <supernatural64@mail.ru>
2022-09-09 14:29:06 +0400

    task005

---
+++ task005/Program.cs
@@ -0,0 +1,11 @@
+﻿//Написать программу вычисления функции Аккермана
+
+
+int Akkerman(int n,int m)
+{ 
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Akkerman(n - 1, 1);
+    if (n > 0 && m > 0) return Akkerman(n - 1, Akkerman(n, m - 1));
+    return Akkerman(n,m);
+}
+Console.WriteLine(Akkerman(2,1));

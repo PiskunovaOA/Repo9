edbe6de7fedd5a90e6fae5f30deef3be08dbd272
Olga <supernatural64@mail.ru>
2022-09-09 13:54:06 +0400

    task001

---
+++ task001/Program.cs
@@ -0,0 +1,17 @@
+﻿//Показать натуральные числа от N до 1, N задано
+
+
+int Numbers(int n,int count)
+    {
+	if (n < 1)
+	{
+	    return count;
+	}
+	else Console.Write(" {0} ",n);
+		n--;
+	return Numbers(n,count);
+    }
+
+	Console.Write(" Показать натуральные числа : ");
+	int n= Convert.ToInt32(Console.ReadLine());
+	Numbers(n,1);
